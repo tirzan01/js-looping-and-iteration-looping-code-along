f8acc7ffd1f02fea3ac9d9cc8b9653c5fd48fc32
thomas <thomasmonta98@gmail.com>
2021-05-23 12:53:24 +1000

    done with assingment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(name, event){
+    let message = []
+    for (let i = 0; i < name.length; i++) {
+        message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return message
+}
+
+function countDown(integer){
+    while (integer>=0){
+        console.log(integer)
+        integer--
+    }
+}
